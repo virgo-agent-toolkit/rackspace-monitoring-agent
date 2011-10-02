13733a55674350f8fa56f9bfc53ccd2e4d3f0429
Paul Querna <pquerna@apache.org>
2011-10-02 05:29:58 -0400

    just make using the existing header files easier by propogating a cpppaths variable for each static lib, and fixup missing file from liblua

--- deps/SConscript-lua.py
+++ deps/SConscript-lua.py
@@ -10,7 +10,9 @@ luasrc = Split("""  lapi.o lcode.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o l
                     lundump.o lvm.o lzio.o
 
                     lauxlib.o lbaselib.o ldblib.o liolib.o lmathlib.o loslib.o ltablib.o
-                    lstrlib.o loadlib.o linit.o""")
+                    lstrlib.o loadlib.o linit.o
+
+                    lctype.o""")
 
 def transform_lua(e):
   e = e.replace(".o", ".c")
@@ -23,18 +25,7 @@ if env["PLATFORM"] != "win32":
   lenv.Append(CFLAGS=['-DLUA_USE_POPEN'])
 
 targets['static'] = lenv.StaticLibrary('libluastatic', source = luasrc)
-
-headers = Split("""
-lua/src/lapi.h		lua/src/ldo.h		lua/src/lmem.h		lua/src/lstring.h	lua/src/lualib.h
-lua/src/lauxlib.h	lua/src/lfunc.h		lua/src/lobject.h	lua/src/ltable.h	lua/src/lundump.h
-lua/src/lcode.h		lua/src/lgc.h		lua/src/lopcodes.h	lua/src/ltm.h		lua/src/lvm.h
-lua/src/lctype.h	lua/src/llex.h		lua/src/lparser.h	lua/src/lua.h		lua/src/lzio.h
-lua/src/ldebug.h	lua/src/llimits.h	lua/src/lstate.h	lua/src/luaconf.h
-""")
-
-headers = [lenv.File(x) for x in headers]
-
-targets['headers'] = headers
+targets['cpppaths'] = ['#deps/lua/src']
 
 lenv = env.Clone()
 lenv.PrependUnique(LIBS=[targets['static']])
