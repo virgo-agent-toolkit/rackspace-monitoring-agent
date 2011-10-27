163cbcd90b528c8d4af2c04293c77a085650ef5b
Paul Querna <pquerna@apache.org>
2011-10-27 00:22:25 -0700

    configure the monitoring agent to look for a lua pack file

--- agents/monitoring/monitoring.c
+++ agents/monitoring/monitoring.c
@@ -7,6 +7,9 @@ int main(int argc, char* argv[])
 
   virgo_create(&v);
 
+  /* TODO: read path from config file */
+  virgo_conf_lua_load_path(v, "./monitoring.lpack");
+
   virgo_run(v);
 
   virgo_destroy(v);
