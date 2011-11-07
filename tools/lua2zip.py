8581dbef50545847f558d40548da15f86d372a4d
Paul Querna <pquerna@apache.org>
2011-11-06 19:20:41 -0800

    Bundle in most of luvit

--- tools/lua2zip.py
+++ tools/lua2zip.py
@@ -8,9 +8,13 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 target = sys.argv[1]
 sources = sys.argv[2:]
-root = os.path.commonprefix(sources)
+# we used to support subdirectories, now every .lua
+# file is put into the root of the zip, reconsider this
+# someday?
+# root = os.path.commonprefix(sources)
 
 z = ZipFile(target, 'w', ZIP_DEFLATED)
 for source in sources:
-    z.write(source, source[len(root):])
+    # z.write(source, source[len(root):])
+    z.write(source, os.path.basename(source))
 z.close()
