fd47b0c57b92909dc1ea4738893bbb85dda80c59
Paul Querna <pquerna@apache.org>
2011-11-06 22:19:03 -0800

    include a webserver to give people a better idea...

--- agents/monitoring/lua/monitoring-agent.lua
+++ agents/monitoring/lua/monitoring-agent.lua
@@ -5,6 +5,19 @@ print ("Hello World!")
 
 function init.run()
   print ("hey this is kinda working")
+  local HTTP = require("http")
+  local Utils = require("utils")
+
+  HTTP.create_server("0.0.0.0", 8080, function (req, res)
+    local body = Utils.dump({req=req,headers=req.headers}) .. "\n"
+    res:write_head(200, {
+      ["Content-Type"] = "text/plain",
+      ["Content-Length"] = #body
+    })
+    res:finish(body)
+  end)
+
+  print("Server listening at http://localhost:8080/")
 end
 
 return init
