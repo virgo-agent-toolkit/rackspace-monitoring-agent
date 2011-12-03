fc7ed8125c1da78fa997a0ac776ad0fdc10d407d
Paul Querna <pquerna@apache.org>
2011-12-02 22:22:16 -0800

    show command, use subprocess not os.system so path escaping is done better

--- tools/build.py
+++ tools/build.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 import sys
 
 # TODO: release/debug
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if sys.platform != "win32":
-  os.system("make -C %s" % root)
+    cmd = ['make', '-C', root]
 else:
-  os.system("devenv.exe /build Debug virgo.sln /project monitoring-agent.vcproj")
+    cmd = ['devenv.exe', '/build', 'Debug', 'virgo.sln', '/project', 'monitoring-agent.vcproj']
+
+print ' '.join(cmd)
+subprocess.call(cmd, shell=True)
 
