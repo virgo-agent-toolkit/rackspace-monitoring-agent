15a63c05860be6aa8462e5392f71fd8e9f17dbef
Paul Querna <pquerna@apache.org>
2011-12-19 00:53:58 -0800

    demo some sigar info fetching

--- agents/monitoring/lua/monitoring-agent.lua
+++ agents/monitoring/lua/monitoring-agent.lua
@@ -1,12 +1,14 @@
 
 local init = {}
 
-print ("Hello World!")
-
 function init.run()
-  print ("hey this is kinda working")
   local HTTP = require("http")
   local Utils = require("utils")
+  local s = sigar:new()
+  local sysinfo = s:sysinfo()
+  local cpus = s:cpus()
+  local netifs = s:netifs()
+  local i = 1;
 
   HTTP.create_server("0.0.0.0", 8080, function (req, res)
     local body = Utils.dump({req=req,headers=req.headers}) .. "\n"
@@ -16,6 +18,22 @@ function init.run()
     })
     res:finish(body)
   end)
+
+  print("sigar.sysinfo = ".. Utils.dump(sysinfo))
+
+  while i <= #cpus do
+    print("sigar.cpus[".. i .."].info = ".. Utils.dump(cpus[i]:info()))
+    print("sigar.cpus[".. i .."].data = ".. Utils.dump(cpus[i]:data()))
+    i = i + 1
+  end
+
+  i = 1;
+
+  while i <= #netifs do
+    print("sigar.netifs[".. i .."].info = ".. Utils.dump(netifs[i]:info()))
+    print("sigar.netifs[".. i .."].usage = ".. Utils.dump(netifs[i]:usage()))
+    i = i + 1
+  end
 
   print("Server listening at http://localhost:8080/")
 end
