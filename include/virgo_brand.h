564d825c6770b6adfe7770faf79fada891f8245f
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-17 18:14:31 -0600

    add branding header

---
+++ include/virgo_brand.h
@@ -0,0 +1,14 @@
+#ifndef _virgo_brand_h_
+#define _vrigo_brand_h_
+
+#define VIRGO_DEFAULT_NAME "rackspace"
+#define VIRGO_DEFAULT_CONFIG_WINDOWS_DIRECTORY \
+  "Rackspace Agent"
+#define VIRGO_DEFAULT_CONFIG_FILENAME \
+  VIRGO_DEFAULT_NAME \
+  ".cfg"
+#define VIRGO_DEFAULT_CONFIG_UNIX_PATH \
+  "/etc/" \
+  VIRGO_DEFAULT_CONFIG_FILENAME
+
+#endif
