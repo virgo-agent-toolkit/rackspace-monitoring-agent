485a9818e5f57e50c6c6adc6355143a979a5a0b9
Brandon Philips <brandon.philips@rackspace.com>
2012-01-19 12:11:20 -0800

    lua_modules: upgrade bourbon

--- lua_modules/bourbon/test.lua
+++ lua_modules/bourbon/test.lua
@@ -1,5 +1,28 @@
 #!/usr/bin/env luvit
 
+local FS = require('fs')
+local Path = require('path')
 local bourbon = require('./')
+local async = require('async')
 
-bourbon.run(require('./tests/asserts'))
+local fmt = require('string').format
+
+local TEST_PATH = './tests'
+
+local function runit(filename, callback)
+  local modname = './' .. Path.join(TEST_PATH, filename)
+  process.stdout:write(fmt('Executing test module [%s]\n\n', modname))
+  bourbon.run(nil, require(modname), function(err)
+    process.stdout:write('\n')
+    callback()
+  end)
+end
+
+FS.readdir(TEST_PATH, function(err, files)
+  if err then
+    p(err)
+    return
+  end
+  async.forEachSeries(files, runit, function(err) end)
+end)
+
