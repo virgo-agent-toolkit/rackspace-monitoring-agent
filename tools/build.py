8744afcdeb08d0fb08b1522808a4514a400eae4d
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-20 14:48:21 -0600

    upgrade build.py

--- tools/build.py
+++ tools/build.py
@@ -7,10 +7,42 @@ import sys
 # TODO: release/debug
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if sys.platform != "win32":
-    cmd = ['make', '-C', root]
-else:
-    cmd = ['tools\win_build.bat']
+build_dir = os.path.join(root, 'out')
 
-print ' '.join(cmd)
-sys.exit(subprocess.call(cmd, shell=True))
+def build():
+  if sys.platform != "win32":
+      cmd = 'make -C %s' % build_dir
+  else:
+      cmd = 'tools\win_build.bat'
+
+  print cmd
+  sys.exit(subprocess.call(cmd, shell=True))
+
+def test():
+  agent = os.path.join(root, 'monitoring-agent')
+  cmd = '%s --zip monitoring-test.zip -e tests -c docs/sample.state' % agent
+  print cmd
+  rc = subprocess.call(cmd, shell=True)
+  sys.exit(rc)
+
+commands = {
+  'build': build,
+  'test': test,
+}
+
+def usage():
+  print('Usage: build.py [%s]' % ', '.join(commands.keys()))
+  sys.exit(1)
+
+if len(sys.argv) != 2:
+  usage()
+
+ins = sys.argv[1]
+if not commands.has_key(ins):
+  print('Invalid command: %s' % ins)
+  sys.exit(1)
+
+print('Running %s' % ins)
+cmd = commands.get(ins)
+cmd()
+
