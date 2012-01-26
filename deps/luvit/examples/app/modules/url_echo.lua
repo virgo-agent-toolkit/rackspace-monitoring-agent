bfea1cffeed08e66c720fa5a542fcc7e51455af1
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-26 15:12:31 -0800

    remove luvit fork

--- deps/luvit/examples/app/modules/url_echo.lua
+++
@@ -1,4 +0,0 @@
-return function(req, res, continue)
-  res:finish(req.url .. "\n")
-end
-
--- deps/luvit/examples/native/vector.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_VECTOR
-#define LIB_VECTOR
-
-#include "lua.h"
-#include "lauxlib.h"
-
-LUALIB_API int luaopen_vector (lua_State *L);
-
-#endif
