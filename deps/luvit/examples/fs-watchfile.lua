b9b6b53e6ee3083828356cfcfef16db5a7363462
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-26 15:12:31 -0800

    remove luvit fork

--- deps/luvit/examples/fs-watchfile.lua
+++
@@ -1,8 +0,0 @@
-local UV = require('uv')
-
-local watcher = UV.new_fs_watcher(".")
-watcher:set_handler("change", function (event, path)
-  p("on_change", {event=event,path=path})
-end);
-
-p(watcher)
