69103f25ba3d3551149c964750ac0532bd602d24
Paul Querna <pquerna@apache.org>
2012-02-11 22:58:43 +0000

    synchroize most of virgo-init.lua with luvit.lua

--- lib/lua/virgo-init.lua
+++ lib/lua/virgo-init.lua
@@ -11,21 +11,21 @@ _G.debug = nil
 _G.table = nil
 _G.loadfile = nil
 _G.dofile = nil
+--_oldprint = print
 _G.print = nil
 
 -- Load libraries used in this file
-local Debug = require('debug')
-
+-- Load libraries used in this file
+local debugm = require('debug')
 local uv = require('uv')
-local Env = require('env')
-
-local Table = require('table')
-local Utils = require('utils')
-local FS = require('fs')
+local env = require('env')
+local table = require('table')
+local utils = require('utils')
+local fs = require('fs')
 local Tty = require('tty').Tty
 local Emitter = require('core').Emitter
-local Constants = require('constants')
-local Path = require('path')
+local constants = require('constants')
+local path = require('path')
 local LVFS = VFS
 _G.VFS = nil
 
@@ -56,17 +56,13 @@ _G.HTTP_VERSION = nil
 
 local vfs = LVFS.open()
 
-process = Emitter:new()
-
-function process.exit(exit_code, clean)
+function process.exit(exit_code)
   process:emit('exit', exit_code)
-  if (clean ~= 1) then
-    exitProcess(exit_code or 0)
-  end
+  exitProcess(exit_code or 0)
 end
 
 function process:addHandlerType(name)
-  local code = Constants[name]
+  local code = constants[name]
   if code then
     uv.activateSignalHandler(code)
     uv.unref()
@@ -98,35 +94,25 @@ end
 hide("_G")
 hide("exit_process")
 
--- Remove the cwd based loaders, we don't want them
-local builtin_loader = package.loaders[1]
-package.loaders = nil
-package.path = nil
-package.cpath = nil
-package.searchpath = nil
-package.seeall = nil
-package.config = nil
-_G.module = nil
-
-
 -- Ignore sigpipe and exit cleanly on SIGINT and SIGTERM
 -- These shouldn't hold open the event loop
-if virgo_os ~= "win" then
-  uv.activateSignalHandler(Constants.SIGPIPE)
+if luvit_os ~= "win" then
+  uv.activateSignalHandler(constants.SIGPIPE)
   uv.unref()
-  uv.activateSignalHandler(Constants.SIGINT)
+  uv.activateSignalHandler(constants.SIGINT)
   uv.unref()
-  uv.activateSignalHandler(Constants.SIGTERM)
+  uv.activateSignalHandler(constants.SIGTERM)
   uv.unref()
 end
 
 -- Load the tty as a pair of pipes
 -- But don't hold the event loop open for them
 process.stdin = Tty:new(0)
-uv.unref()
 process.stdout = Tty:new(1)
-uv.unref()
 local stdout = process.stdout
+uv.unref()
+uv.unref()
+
 
 -- Replace print
 function print(...)
@@ -137,7 +123,7 @@ function print(...)
     arguments[i] = tostring(arguments[i])
   end
 
-  stdout:write(Table.concat(arguments, "\t") .. "\n")
+  stdout:write(table.concat(arguments, "\t") .. "\n")
 end
 
 -- A nice global data dumper
@@ -146,30 +132,30 @@ function p(...)
   local arguments = { ... }
 
   for i = 1, n do
-    arguments[i] = Utils.dump(arguments[i])
+    arguments[i] = utils.dump(arguments[i])
   end
 
-  stdout:write(Table.concat(arguments, "\t") .. "\n")
+  stdout:write(table.concat(arguments, "\t") .. "\n")
 end
 
-hide("print_stderr")
+hide("printStderr")
 -- Like p, but prints to stderr using blocking I/O for better debugging
 function debug(...)
   local n = select('#', ...)
   local arguments = { ... }
 
   for i = 1, n do
-    arguments[i] = Utils.dump(arguments[i])
+    arguments[i] = utils.dump(arguments[i])
   end
 
-  printStderr(Table.concat(arguments, "\t") .. "\n")
+  printStderr(table.concat(arguments, "\t") .. "\n")
 end
 
 
 -- Add global access to the environment variables using a dynamic table
 process.env = setmetatable({}, {
   __pairs = function (table)
-    local keys = Env.keys()
+    local keys = env.keys()
     local index = 0
     return function (...)
       index = index + 1
@@ -180,18 +166,16 @@ process.env = setmetatable({}, {
     end
   end,
   __index = function (table, name)
-    return Env.get(name)
+    return env.get(name)
   end,
   __newindex = function (table, name, value)
     if value then
-      Env.set(name, value, 1)
+      env.set(name, value, 1)
     else
-      Env.unset(name)
+      env.unset(name)
     end
   end
 })
-
-local global_meta = {__index=_G}
 
 -- This is called by all the event sources from C
 -- The user can override it to hook into event sources
@@ -199,90 +183,153 @@ function eventSource(name, fn, ...)
   local args = {...}
   return assert(xpcall(function ()
     return fn(unpack(args))
-  end, Debug.traceback))
+  end, debugm.traceback))
 end
 
-local function myloadfile(path)
-  if not vfs:exists(path) then return end
+error_meta = {__tostring=function(table) return table.message end}
 
-  if package.loaded[path] then
+local global_meta = {__index=_G}
+
+
+local function partialRealpath(filepath)
+  -- Do some minimal realpathing
+  local link
+  link = fs.lstatSync(filepath).is_symbolic_link and fs.readlinkSync(filepath)
+  while link do
+    filepath = path.resolve(path.dirname(filepath), link)
+    link = fs.lstatSync(filepath).is_symbolic_link and fs.readlinkSync(filepath)
+  end
+  return path.normalize(filepath)
+end
+
+
+local function myloadfile(filepath)
+  if not vfs:exists(filepath) then return end
+  -- Not done by luvit, we don't have synlinks in the zip file.
+  -- filepath = partialRealpath(filepath)
+
+  if package.loaded[filepath] then
     return function ()
-      return package.loaded[path]
+      return package.loaded[filepath]
     end
   end
 
-  local code = vfs:read(path)
+  local code = vfs:read(filepath)
 
-  local fn = assert(loadstring(code, '@' .. path))
-  local dirname = Path.dirname(path)
+  -- TODO: find out why inlining assert here breaks the require test
+  local fn = loadstring(code, '@' .. filepath)
+  assert(fn)
+  local dirname = path.dirname(filepath)
+  local realRequire = require
   setfenv(fn, setmetatable({
-    __filename = path,
+    __filename = filepath,
     __dirname = dirname,
-    require = function (path)
-      return virgo_require(path, dirname)
+    require = function (filepath)
+      return realRequire(filepath, dirname)
     end,
   }, global_meta))
   local module = fn()
-  package.loaded[path] = module
+  package.loaded[filepath] = module
   return function() return module end
 end
 
+local function myloadlib(filepath)
+  if not vfs:exists(filepath) then return end
+
+  filepath = partialRealpath(filepath)
+
+  if package.loaded[filepath] then
+    return function ()
+      return package.loaded[filepath]
+    end
+  end
+
+  local name = path.basename(filepath)
+  if name == "init.luvit" then
+    name = path.basename(path.dirname(filepath))
+  end
+  local base_name = name:sub(1, #name - 6)
+  package.loaded[filepath] = base_name -- Hook to allow C modules to find their path
+  local fn, error_message = package.loadlib(filepath, "luaopen_" .. base_name)
+  if fn then
+    local module = fn()
+    package.loaded[filepath] = module
+    return function() return module end
+  end
+  error(error_message)
+end
+
 -- tries to load a module at a specified absolute path
-local function load_module(path, verbose)
+local function loadModule(filepath, verbose)
 
   -- First, look for exact file match if the extension is given
-  local extension = Path.extname(path)
+  local extension = path.extname(filepath)
   if extension == ".lua" then
-    return myloadfile(path)
+    return myloadfile(filepath)
+  end
+  if extension == ".luvit" then
+    return myloadlib(filepath)
   end
 
   -- Then, look for module/package.lua config file
-  if vfs:exists(path .. "/package.lua") then
-    local metadata = load_module(path .. "/package.lua")()
+  if vfs:exists(filepath .. "/package.lua") then
+    local metadata = loadModule(filepath .. "/package.lua")()
     if metadata.main then
-      return load_module(Path.join(path, metadata.main))
+      return loadModule(path.join(filepath, metadata.main))
     end
   end
 
   -- Try to load as either lua script or binary extension
-  local fn = myloadfile(path .. ".lua") or myloadfile(path .. "/init.lua")
+  local fn = myloadfile(filepath .. ".lua") or myloadfile(filepath .. "/init.lua")
+          or myloadlib(filepath .. ".luvit") or myloadlib(filepath .. "/init.luvit")
   if fn then return fn end
 
-  return "\n\tCannot find module " .. path
+  return "\n\tCannot find module " .. filepath
 end
 
+-- Remove the cwd based loaders, we don't want them
+local builtinLoader = package.loaders[1]
+package.loaders = nil
+package.path = nil
+package.cpath = nil
+package.searchpath = nil
+package.seeall = nil
+package.config = nil
+_G.module = nil
 
-function virgo_require(path, dirname)
-  if not dirname then dirname = '' end
+function require(filepath, dirname)
+  if not dirname then dirname = base_path end
 
   -- Absolute and relative required modules
-  local first = path:sub(1, 1)
+  local first = filepath:sub(1, 1)
   local absolute_path
   if first == "/" then
-    absolute_path = Path.normalize(path)
+    absolute_path = path.normalize(filepath)
   elseif first == "." then
-    absolute_path = Path.join(dirname, path)
+    absolute_path = path.join(dirname, filepath)
   end
   if absolute_path then
-    local loader = load_module(absolute_path)
+    local loader = loadModule(absolute_path)
     if type(loader) == "function" then
       return loader()
     else
-      error("Failed to find module '" .. path .."'")
+      error("Failed to find module '" .. filepath .."'")
     end
   end
 
   local errors = {}
 
   -- Builtin modules
-  local module = package.loaded[path]
+  local module = package.loaded[filepath]
   if module then return module end
-  if path:find("^[a-z_]+$") then
-    local loader = builtin_loader(path)
+  if filepath:find("^[a-z_]+$") then
+    local loader = builtinLoader(filepath)
     if type(loader) == "function" then
       module = loader()
-      package.loaded[path] = module
+      package.loaded[filepath] = module
       return module
+    else
+      errors[#errors + 1] = loader
     end
   end
 
@@ -290,8 +337,8 @@ function virgo_require(path, dirname)
   local dir = dirname .. "/"
   repeat
     dir = dir:sub(1, dir:find("/[^/]*$") - 1)
-    local full_path = dir .. "/modules/" .. path
-    local loader = load_module(full_path)
+    local full_path = dir .. "/modules/" .. filepath
+    local loader = loadModule(dir .. "/modules/" .. filepath)
     if type(loader) == "function" then
       return loader()
     else
@@ -299,13 +346,9 @@ function virgo_require(path, dirname)
     end
   until #dir == 0
 
-  error("Failed to find module '" .. path .."'" .. Table.concat(errors, ""))
+  error("Failed to find module '" .. filepath .."'" .. table.concat(errors, ""))
 
 end
-
-error_meta = {__tostring=function(table) return table.message end}
-
-require = virgo_require
 
 local virgo_init = {}
 
@@ -313,7 +356,6 @@ function virgo_init.run(name)
   local mod = require(name)
 
   mod.run()
-
   -- Start the event loop
   uv.run()
   -- trigger exit handlers and exit cleanly
