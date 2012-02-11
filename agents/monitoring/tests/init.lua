4f1cadbeb5cae86e56074bf3baee7da84fec0b8f
Paul Querna <pquerna@apache.org>
2012-02-11 22:40:34 +0000

    use pcall to catch errors on require

--- agents/monitoring/tests/init.lua
+++ agents/monitoring/tests/init.lua
@@ -5,15 +5,24 @@ local fmt = require('string').format
 local exports = {}
 
 local function runit(modname, callback)
+  local status, mod = pcall(require, modname)
+  if status ~= true then
+    process.stdout:write(fmt('Error loading test module [%s]: %s\n\n', modname, mod))
+    callback(mod)
+  end
   process.stdout:write(fmt('Executing test module [%s]\n\n', modname))
-  bourbon.run(nil, require(modname), function(err)
+  bourbon.run(nil, mod, function(err)
     process.stdout:write('\n')
     callback()
   end)
 end
 
 exports.run = function()
-  async.forEachSeries({"./agent-protocol", "./crypto"}, runit, function(err) end)
+  async.forEachSeries({"./agent-protocol", "./crypto"}, runit, function(err)
+    if err then
+      process.exit(1)
+    end
+  end)
 end
 
 return exports
