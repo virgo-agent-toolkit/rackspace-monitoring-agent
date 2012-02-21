57cd8819ed28f25d22e7d29589d7f66c548069d4
Tomaz Muraus <tomaz@tomaz.me>
2012-02-21 15:10:08 -0800

    Add splitAddress utility function.

---
+++ agents/monitoring/lua/lib/util/misc.lua
@@ -0,0 +1,35 @@
+--[[
+Copyright 2012 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+--]]
+
+-- Split address in IP:port format and return an array of [ip, port]
+function splitAddress(address)
+  local start, result
+  start, _ = address:find(':')
+
+  if not start then
+    return null
+  end
+
+  result = {}
+  result[1] = address:sub(0, start - 1)
+  result[2] = tonumber(address:sub(start + 1))
+  return result
+end
+
+--[[ Exports ]]--
+local exports = {}
+exports.splitAddress = splitAddress
+return exports
