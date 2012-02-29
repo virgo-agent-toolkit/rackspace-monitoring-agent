93fd32dd3ab6fd2cba33060d514b9db4c43fee1f
Tomaz Muraus <tomaz@tomaz.me>
2012-02-28 22:07:44 -0800

    Add a test case for mkdirp.

---
+++ agents/monitoring/tests/fs/init.lua
@@ -0,0 +1,56 @@
+--[[
+Copyright 2012 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+--]]
+
+local math = require('math')
+local table = require('table')
+local fs = require('fs')
+local os = require('os')
+
+local exports = {}
+
+local misc = require('monitoring/lib/util/misc')
+local fsUtil = require('monitoring/lib/util/fs')
+
+exports['test_mkdirp'] = function(test, asserts)
+  local separator, component, components, fulPath
+
+  components = {}
+
+  if os.type() == 'win32' then
+    separator = '\\'
+    components[1] = 'C:'
+  else
+    separator = '/'
+    components[1] = '/tmp'
+  end
+
+  for i=2,5 do
+    component = 'test' .. tostring(math.random(0, 1000))
+    components[i] = component
+  end
+
+  fullPath = table.concat(components, separator)
+
+  asserts.not_ok(fs.existsSync(fullPath))
+
+  fsUtil.mkdirp(fullPath, 0755, function(err)
+    asserts.ok(not err)
+    asserts.ok(fs.existsSync(fullPath))
+    test.done()
+  end)
+end
+
+return exports
