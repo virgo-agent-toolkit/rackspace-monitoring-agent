4ce499d4245540aaa77a5b11a7622f82251c63bc
Brandon Philips <brandon.philips@rackspace.com>
2012-03-15 11:21:32 -0700

    monitoring: add a no verify flag
    Infrastructure for having a no verify flag so that if people are debugging or
    trying out things they don't need to verify agent zip file downloads.

--- agents/monitoring/lua/init.lua
+++ agents/monitoring/lua/init.lua
@@ -19,19 +19,22 @@ local logging = require('logging')
 local Entry = {}
 
 local argv = require("options")
-  :usage("Usage: ")
-  :describe("e", "entry module")
-  :describe("s", "state directory path")
-  :argv("he:c:s:")
+  .usage("Usage: ")
+  .describe("n", "Do not verify Zip Bundle.")
+  .describe("e", "entry module")
+  .describe("s", "state directory path")
+  .argv("ne:s:")
 
 function Entry.run()
   local mod = argv.args.e or 'monitoring-agent'
   mod = './' .. mod
 
+  local verify = not argv.args.n
+
   logging.log(logging.INFO, 'Running Module ' .. mod)
 
   local err, msg = pcall(function()
-    require(mod).run({stateDirectory = argv.args.s})
+    require(mod).run({stateDirectory = argv.args.s, verify = verify})
   end)
 
   if err == false then
