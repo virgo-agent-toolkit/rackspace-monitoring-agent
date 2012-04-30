9e5c222037882a6eb8cbfb5d4b267936081d9797
Ryan Phillips <ryan.phillips@rackspace.com>
2012-04-30 09:28:31 -0500

    add srv record lookups

--- agents/monitoring/lua/lib/util/constants.lua
+++ agents/monitoring/lua/lib/util/constants.lua
@@ -1,8 +1,14 @@
 local exports = {}
+
+exports.GC_INTERVAL = 5 * 1000
+
 exports.CONNECT_TIMEOUT = 6000
 exports.SOCKET_TIMEOUT = 10000
 exports.PING_INTERVAL_JITTER = 7000
 
 exports.DATACENTER_MAX_DELAY = 5 * 60 * 1000 -- max connection delay
 exports.DATACENTER_MAX_DELAY_JITTER = 7000
+
+exports.SRV_RECORD_FAILURE_DELAY = 15 * 1000
+exports.SRV_RECORD_FAILURE_DELAY_JITTER = 15 * 1000
 return exports
