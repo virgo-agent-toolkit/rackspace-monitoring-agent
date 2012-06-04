7d4d2fe4c8a8a32d13bbfd35ad30cd981eee35d3
Ryan Phillips <ryan.phillips@rackspace.com>
2012-06-04 15:14:39 -0500

    whitelist available fields

--- agents/monitoring/lua/lib/info.lua
+++ agents/monitoring/lua/lib/info.lua
@@ -25,18 +25,38 @@ local CPUInfo = Info:extend()
 function CPUInfo:initialize()
   Info.initialize(self)
   local cpus = self._s:cpus()
-  self._params = {}
   for i=1, #cpus do
     local info = cpus[i]:info()
     local data = cpus[i]:data()
     local bucket = 'cpu.' .. i - 1
+    local data_fields = {
+      'idle',
+      'irq',
+      'nice',
+      'soft_irq',
+      'stolen',
+      'sys',
+      'total',
+      'user',
+      'wait'
+    }
+    local info_fields = {
+      'cache_size',
+      'cores_per_socket',
+      'mhz',
+      'model',
+      'total_cores',
+      'total_sockets',
+      'vendor'
+    }
 
     self._params[bucket] = {}
-    for k, v in pairs(info) do
-      self._params[bucket][k] = v
+
+    for k, v in pairs(data_fields) do
+      self._params[bucket][v] = data[v]
     end
-    for k, v in pairs(data) do
-      self._params[bucket][k] = v
+    for k, v in pairs(info_fields) do
+      self._params[bucket][v] = info[v]
     end
   end
 end
@@ -47,13 +67,25 @@ function DiskInfo:initialize()
   Info.initialize(self)
   local disks = self._s:disks()
   local name, usage
+  local usage_fields = {
+    'queue',
+    'read_bytes',
+    'reads',
+    'rtime',
+    'service_time',
+    'snaptime',
+    'time',
+    'write_bytes',
+    'writes',
+    'wtime'
+  }
   for i=1, #disks do
     name = disks[i]:name()
     usage = disks[i]:usage()
-    if usage then
+    if name and usage then
       self._params[name] = {}
-      for key, value in pairs(usage) do
-        self._params[name][key] = value
+      for k, v in pairs(usage_fields) do
+        self._params[name][v] = usage[v]
       end
     end
   end
@@ -63,9 +95,21 @@ end
 local MemoryInfo = Info:extend()
 function MemoryInfo:initialize()
   Info.initialize(self)
-  local meminfo = self._s:mem()
-  for key, value in pairs(meminfo) do
-    self._params[key] = value
+  local data = self._s:mem()
+  local data_fields = {
+    'actual_free',
+    'actual_used',
+    'free',
+    'free_percent',
+    'ram',
+    'total',
+    'used',
+    'used_percent'
+  }
+  if data then
+    for k, v in pairs(data_fields) do
+      self._params[v] = data[v]
+    end
   end
 end
 
@@ -78,12 +122,41 @@ function NetworkInfo:initialize()
     local info = netifs[i]:info()
     local usage = netifs[i]:usage()
     local name = info.name
+    local info_fields = {
+      'address',
+      'broadcast',
+      'destination',
+      'flags',
+      'hwaddr',
+      'metric',
+      'mtu',
+      'name',
+      'netmask',
+      'type'
+    }
+    local usage_fields = {
+      'rx_packets',
+      'rx_bytes',
+      'rx_errors',
+      'rx_overruns',
+      'rx_dropped',
+      'rx_frame',
+      'tx_packets',
+      'tx_bytes',
+      'tx_errors',
+      'tx_overruns',
+      'tx_dropped',
+      'tx_collisions',
+      'tx_carrier',
+      'speed'
+    }
+
     self._params[name] = {}
-    for k, v in pairs(info) do
-      self._params[name][k] = v
+    for k, v in pairs(info_fields) do
+      self._params[name][v] = info[v]
     end
-    for k, v in pairs(usage) do
-      self._params[name][k] = v
+    for k, v in pairs(usage_fields) do
+      self._params[name][v] = usage[v]
     end
   end
 end
@@ -107,8 +180,13 @@ function ProcessInfo:initialize()
 
     local t, msg = proc:exe()
     if t then
-      for k, v in pairs(t) do
-        self._params[pid].exe[k] = v
+      local exe_fields = {
+        'name',
+        'cwd',
+        'root'
+      }
+      for k, v in pairs(exe_fields) do
+        self._params[pid].exe[v] = t[v]
       end
     else
       self._params[pid].exe = 'Unavailable'
@@ -116,8 +194,14 @@ function ProcessInfo:initialize()
 
     t, msg = proc:time()
     if t then
-      for k, v in pairs(t) do
-        self._params[pid].time[k] = v
+      local time_fields = {
+        'start_time',
+        'user',
+        'sys',
+        'total'
+      }
+      for k, v in pairs(time_fields) do
+        self._params[pid].time[v] = t[v]
       end
     else
       self._params[pid].time = 'Unavailable'
@@ -125,8 +209,17 @@ function ProcessInfo:initialize()
 
     t, msg = proc:state()
     if t then
-      for k, v in pairs(t) do
-        self._params[pid].state[k] = v
+      local proc_fields = {
+        'name',
+        'ppid',
+        'tty',
+        'priority',
+        'nice',
+        'processor',
+        'threads'
+      }
+      for k, v in pairs(proc_fields) do
+        self._params[pid].state[v] = t[v]
       end
     else
       self._params[pid].state = 'Unavailable'
@@ -134,8 +227,16 @@ function ProcessInfo:initialize()
 
     t, msg = proc:mem()
     if t then
-      for k, v in pairs(t) do
-        self._params[pid].memory[k] = v
+      local memory_fields = {
+        'size',
+        'resident',
+        'share',
+        'major_faults',
+        'minor_faults',
+        'page_faults'
+      }
+      for k, v in pairs(memory_fields) do
+        self._params[pid].memory[v] = t[v]
       end
     else
       self._params[pid].memory = 'Unavailable'
