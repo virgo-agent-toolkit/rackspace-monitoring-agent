1e8322296feaa16c4c9fc512a8f20f1a2eb68eb3
Tomaz Muraus <tomaz@tomaz.me>
2012-06-05 23:50:51 -0700

    Add utility requesy function.

--- agents/monitoring/lua/lib/http/utils.lua
+++ agents/monitoring/lua/lib/http/utils.lua
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 --]]
 
+local http = require('http')
+local parse = require('url').parse
 local JSON = require('json')
 
 local exports = {}
@@ -46,7 +48,46 @@ function returnJson(res, code, data)
   returnResponse(res, code, headers, data)
 end
 
+function request(url, method, headers, payload, options, callback)
+  method = method and method or 'GET'
+  headers = headers and headers or {}
+  payload = payload and payload or ''
+  options = options and options or {}
+
+  local parsed = parse(url)
+  local buffer = ''
+
+  headers['Content-Length'] = #payload
+
+  local client = http.request({
+    host = parsed.hostname,
+    port = parsed.port,
+    path = parsed.pathname,
+    headers = headers
+  },
+
+  function (res)
+    res:on('error', callback)
+
+    res:on('data', function (chunk)
+      buffer = buffer .. chunk
+    end)
+
+    res:on('end', function()
+      if options.parseResponse then
+        buffer = JSON.parse(buffer)
+      end
+
+      res.body = buffer
+      callback(nil, res)
+    end)
+  end)
+
+  client:on('error', callback)
+end
+
 exports.returnResponse = returnResponse
 exports.returnError = returnError
 exports.returnJson = returnJson
+exports.request = request
 return exports
