b4e7b178cb75ea0687a8aa5e30ef4f37cd6e71a4
Brandon Philips <brandon.philips@rackspace.com>
2012-06-13 15:23:25 -0700

    monitoring: move fixtures to base of tests
    add fixtures to the base of tests and add first "invalid-version"
    fixture so we can test shutting down the agent when an invalid version
    hello is given.

---
+++ agents/monitoring/tests/fixtures/protocol/init.lua
@@ -0,0 +1,32 @@
+local path = require('path')
+local fs = require('fs')
+local string = require('string')
+
+local fixtures = nil
+
+local strip_newlines = function(str)
+  return str:gsub("\n", " ")
+end
+
+local load_fixtures = function(dir)
+  local fixtures = {}
+  local files = fs.readdirSync(dir)
+
+  for i, v in ipairs(files) do
+    local _, _, name = string.find(v, '(.*).json')
+    if name ~= nil then
+      local json = fs.readFileSync(path.join(dir, v))
+      json = strip_newlines(json)
+      fixtures[name] = json
+    end
+  end
+
+  return fixtures
+end
+
+local base = path.join('agents', 'monitoring', 'tests', 'fixtures', 'protocol')
+
+fixtures = load_fixtures(base)
+fixtures['invalid-version'] = load_fixtures(path.join(base, 'invalid-version'))
+
+return fixtures
