dfb4a72e8afe21f19855194d56647b9383f125ae
Tomaz Muraus <tomaz@tomaz.me>
2012-06-24 12:41:50 -0700

    Add a test for backward compatbility with cloudkick agent plugins.

--- agents/monitoring/tests/check/init.lua
+++ agents/monitoring/tests/check/init.lua
@@ -255,4 +255,24 @@ exports['test_custom_plugin_dimensions'] = function(test, asserts)
   end)
 end
 
+exports['test_custom_plugin_cloudkick_agent_plugin_backward_compatibility'] = function(test, asserts)
+  constants.DEFAULT_CUSTOM_PLUGINS_PATH = path.join(process.cwd(),
+                      '/agents/monitoring/tests/fixtures/custom_plugins')
+
+  local check = PluginCheck:new({id='foo', period=30, name='plugin_1',
+                                 file='cloudkick_agent_custom_plugin.sh'})
+  asserts.ok(check._lastResults == nil)
+  check:run(function(result)
+    local metrics = result:getMetrics()
+
+    asserts.ok(result ~= nil)
+    asserts.equals(result:getStatus(), 'Service is not responding')
+    asserts.equals(result:getState(), 'available')
+
+    asserts.dequals(metrics['none']['logged_users'], {t='int64', v = '7'})
+    asserts.dequals(metrics['none']['active_processes'], {t='int64', v = '200'})
+    test.done()
+  end)
+end
+
 return exports
