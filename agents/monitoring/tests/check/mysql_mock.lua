e1a9694230b20684ddda5071f353f3b9069c4358
Paul Querna <pquerna@apache.org>
2012-08-20 23:54:26 +0000

    Move row mocking to the base class

--- agents/monitoring/tests/check/mysql_mock.lua
+++ agents/monitoring/tests/check/mysql_mock.lua
@@ -23,8 +23,11 @@ local exports = {}
 local testcases = {}
 
 local MySQLMock = Object:extend()
+-- Note: we can't make this class scope due to how we are invoking our proxy class...
+local RowOffset = 1
 
 function MySQLMock:mysql_init(conn)
+  RowOffset = 1
   return 1
 end
 function MySQLMock:mysql_close(conn)
@@ -53,6 +56,50 @@ function MySQLMock:mysql_free_result(result)
 end
 function MySQLMock:mysql_num_fields(results)
   return 2
+end
+
+
+local RowResult = {
+  {"Aborted_clients", "17"},
+  {"Connections", "60"},
+  {"Innodb_buffer_pool_pages_dirty", "0"},
+  {"Innodb_buffer_pool_pages_flushed", "2"},
+  {"Innodb_buffer_pool_pages_free", "8049"},
+  {"Innodb_buffer_pool_pages_total", "8191"},
+  {"Innodb_row_lock_time", "0"},
+  {"Innodb_row_lock_time_avg", "0"},
+  {"Innodb_row_lock_time_max", "0"},
+  {"Innodb_rows_deleted", "0"},
+  {"Innodb_rows_inserted", "0"},
+  {"Innodb_rows_read", "0"},
+  {"Innodb_rows_updated", "0"},
+  {"Qcache_free_blocks", "1"},
+  {"Qcache_free_memory", "16759696"},
+  {"Qcache_hits", "0"},
+  {"Qcache_inserts", "0"},
+  {"Qcache_lowmem_prunes", "0"},
+  {"Qcache_not_cached", "82"},
+  {"Qcache_queries_in_cache", "0"},
+  {"Qcache_total_blocks", "1"},
+  {"Queries", "590"},
+  {"Threads_connected", "1"},
+  {"Threads_created", "1"},
+  {"Threads_running", "1"},
+  {"Uptime", "3212"},
+}
+
+function MySQLMock:mysql_fetch_row(results)
+  local i = RowOffset
+  RowOffset = i + 1
+  if RowResult[i] ~= nil then
+    local key = RowResult[i][1]
+    local val = RowResult[i][2]
+    local rv = {}
+    rv[0] = ffi.new("char[?]", #key + 1, key)
+    rv[1] = ffi.new("char[?]", #val + 1, val)
+    return rv
+  end
+  return nil
 end
 
 
@@ -89,51 +136,7 @@ function MockNumFields:mysql_num_fields(results)
 end
 testcases['failed_num_fields'] = MockNumFields:new()
 
-local MockRows = MySQLMock:extend()
-local RowOffset = 1
-local RowResult = {
-  {"Aborted_clients", "17"},
-  {"Connections", "60"},
-  {"Innodb_buffer_pool_pages_dirty", "0"},
-  {"Innodb_buffer_pool_pages_flushed", "2"},
-  {"Innodb_buffer_pool_pages_free", "8049"},
-  {"Innodb_buffer_pool_pages_total", "8191"},
-  {"Innodb_row_lock_time", "0"},
-  {"Innodb_row_lock_time_avg", "0"},
-  {"Innodb_row_lock_time_max", "0"},
-  {"Innodb_rows_deleted", "0"},
-  {"Innodb_rows_inserted", "0"},
-  {"Innodb_rows_read", "0"},
-  {"Innodb_rows_updated", "0"},
-  {"Qcache_free_blocks", "1"},
-  {"Qcache_free_memory", "16759696"},
-  {"Qcache_hits", "0"},
-  {"Qcache_inserts", "0"},
-  {"Qcache_lowmem_prunes", "0"},
-  {"Qcache_not_cached", "82"},
-  {"Qcache_queries_in_cache", "0"},
-  {"Qcache_total_blocks", "1"},
-  {"Queries", "590"},
-  {"Threads_connected", "1"},
-  {"Threads_created", "1"},
-  {"Threads_running", "1"},
-  {"Uptime", "3212"},
-}
-
-function MockRows:mysql_fetch_row(results)
-  local i = RowOffset
-  RowOffset = i + 1
-  if RowResult[i] ~= nil then
-    local key = RowResult[i][1]
-    local val = RowResult[i][2]
-    local rv = {}
-    rv[0] = ffi.new("char[?]", #key + 1, key)
-    rv[1] = ffi.new("char[?]", #val + 1, val)
-    return rv
-  end
-  return nil
-end
-testcases['fake_results'] = MockRows:new()
+testcases['fake_results'] = MySQLMock:new()
 
 exports.mock = function(clib)
 
