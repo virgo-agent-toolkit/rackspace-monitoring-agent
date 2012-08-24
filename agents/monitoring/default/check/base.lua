9b159448823d75ccf8ff2918f4f82b969a4cfedc
Ryan Phillips <ryan.phillips@rackspace.com>
2012-08-24 14:24:16 -0500

    remove pattern substitution for metrics

--- agents/monitoring/default/check/base.lua
+++ agents/monitoring/default/check/base.lua
@@ -510,19 +510,9 @@ function CheckResult:serializeAsPluginOutput()
 end
 
 function Metric:initialize(name, dimension, type, value)
-  local pattern = '[^0-9a-zA-Z_]'
-
   self.name = name
   self.dimension = dimension or 'none'
   self.value = tostring(value)
-
-  if self.name then
-    self.name = self.name:gsub(pattern, '_')
-  end
-
-  if self.dimension then
-    self.dimension = self.dimension:gsub(pattern, '_')
-  end
 
   if type then
     if not tableContains(function(v) return type == v end, VALID_METRIC_TYPES) then
