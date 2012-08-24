5a0690090256c14fc8421fe1e6f75d41e1d4f510
Matt Kaniaris <mkaniaris@gmail.com>
2012-08-24 11:06:31 -0700

    Renable the Virgo Debugger
    The virgo_debugger is now once again available in lua land with the command `virgo_debugger()`.  It was previously disabled becasue it did not compile due to problems with logging.
    
    Also, virgo_lua_debugger.c no longer redefines the logErr macro and some unused functions that were generating compiler warnings were removed.

--- include/private/virgo__lua.h
+++ include/private/virgo__lua.h
@@ -43,6 +43,8 @@ int virgo__lua_debug_stackwalk(lua_State *l);
 int virgo__lua_vfs_init(lua_State *L);
 int virgo__lua_logging_open(lua_State *L);
 
+static int luahook_stackwalk(lua_State *L);
+
 #define VIRGO_DEFINE_CONSTANT_ALIAS(L, constant, alias) \
   lua_pushnumber(L, constant);             \
   lua_setfield(L, -2, alias)
