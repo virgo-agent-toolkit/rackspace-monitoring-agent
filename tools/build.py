100fff46b0a923586dd009974db93a26eacefb6f
Brandon Philips <brandon@ifup.org>
2012-09-06 18:17:07 -0700

    tools: build: fixup pep8 errors
    pep8 errors related to spacing for the new LUVIT_BIN env variable

--- tools/build.py
+++ tools/build.py
@@ -8,23 +8,24 @@ import sys
 BUILDTYPE = 'Debug'
 
 if os.environ.get('BUILDTYPE'):
-  BUILDTYPE = os.environ.get('BUILDTYPE')
+    BUILDTYPE = os.environ.get('BUILDTYPE')
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 build_dir = os.path.join(root, 'out')
 
 if sys.platform != "win32":
-  agent = os.path.join(root, 'out', BUILDTYPE, 'monitoring-agent')
+    agent = os.path.join(root, 'out', BUILDTYPE, 'monitoring-agent')
 else:
-  agent = os.path.join(root, BUILDTYPE, 'monitoring-agent.exe')
+    agent = os.path.join(root, BUILDTYPE, 'monitoring-agent.exe')
 
 if sys.platform != "win32":
-  luvit = os.path.join(root, 'out', BUILDTYPE, 'luvit')
+    luvit = os.path.join(root, 'out', BUILDTYPE, 'luvit')
 else:
-  luvit = os.path.join(root, BUILDTYPE, 'luvit.exe')
+    luvit = os.path.join(root, BUILDTYPE, 'luvit.exe')
 
 os.environ['AGENT_BIN'] = agent
 os.environ['LUVIT_BIN'] = luvit
+
 
 def extra_env():
     env = {}
