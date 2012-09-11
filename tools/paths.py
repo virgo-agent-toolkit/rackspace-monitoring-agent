c161212a119c51da38ff63208acc9f1844592f88
Brandon Philips <brandon@ifup.org>
2012-09-11 14:10:34 -0700

    tools: build: move path logic into paths module
    paths.py will either export the LUVIT_BIN and AGENT_BIN environment
    variables if ran or will put these variables into fields in the module.
    
    I needed to do this because I want to be able to run the runner script
    outside of tools/build.py

---
+++ tools/paths.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+agent = None
+luvit = None
+
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+build_dir = os.path.join(root, 'out')
+
+BUILDTYPE = os.environ.get('BUILDTYPE', 'Debug')
+
+if sys.platform != "win32":
+    agent = os.path.join(root, 'out', BUILDTYPE, 'monitoring-agent')
+else:
+    agent = os.path.join(root, BUILDTYPE, 'monitoring-agent.exe')
+
+if sys.platform != "win32":
+    luvit = os.path.join(root, 'out', BUILDTYPE, 'luvit')
+else:
+    luvit = os.path.join(root, BUILDTYPE, 'luvit.exe')
+
+if __name__ == "__main__":
+    os.environ['AGENT_BIN'] = agent
+    os.environ['LUVIT_BIN'] = luvit
