7d99bff318a785602f326eb2b28555726b4536c4
Paul Querna <pquerna@apache.org>
2012-09-27 11:22:39 -0700

    Rewrite mkdir to be more portable

--- agents/monitoring/default/util/fs.lua
+++ agents/monitoring/default/util/fs.lua
@@ -1,27 +1,72 @@
 local path = require('path')
 local fs = require('fs')
+local table = require('table')
+local async = require('async')
 
 local exports = {}
 
-function exports.mkdirp(lpath, mode, callback)
-  lpath = path.resolve('', lpath)
-  fs.mkdir(lpath, mode, function(err)
-    if not err then
-      callback()
-      return
+-- TODO: move to utils
+function reverse ( t )
+  local tout = {}
+
+  for i = #t, 1, -1 do
+    table.insert(tout, t[i])
+  end
+
+  return tout
+end
+
+function mkdirp(lpath, mode, callback)
+  lpath = path.normalize(lpath)
+  local tocreate = {lpath}
+  local last = nil
+  local current = lpath
+
+  while 1 do
+    last = current
+    current = path.dirname(current)
+    if current == "." then
+      break
     end
-    if err.code == 'ENOENT' then
-      exports.mkdirp(path.dirname(lpath), mode, function(err)
-        if err then
-          callback(err)
-        else
-          exports.mkdirp(lpath, mode, callback)
+
+    table.insert(tocreate, current)
+
+    if last == current then
+      break
+    end
+    if current == nil then
+      break
+    end
+  end
+
+  tocreate = reverse(tocreate)
+  async.forEachSeries(tocreate, function (dir, callback)
+    fs.mkdir(dir, mode, function(err)
+        if not err then
+          callback()
+          return
         end
+
+        fs.stat(dir, function(err2, stats)
+          if (err2) then
+            -- Okay, so the path didn't exist, but our first mkdir failed, so return the original error.
+            callback(err)
+            return
+          end
+          -- TOOD: isDir()
+          if (stats.is_directory) then
+            callback()
+            return
+          end
+          callback(err)
+          return
+        end)
       end)
-    else
-      callback(err)
-    end
+  end,
+  function(err)
+    callback(err)
   end)
 end
 
+exports['mkdirp'] = mkdirp
 return exports
