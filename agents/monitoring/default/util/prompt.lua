27692c910eb38d954538e4b9bf6feafb8cbe780b
Ryan Phillips <ryan.phillips@rackspace.com>
2012-10-22 14:06:51 -0500

    cleanup and fix setup
    * retry prompt on empty string

--- agents/monitoring/default/util/prompt.lua
+++ agents/monitoring/default/util/prompt.lua
@@ -1,4 +1,5 @@
 local trim = require('./misc').trim
+local async = require('async')
 
 --[[
 Ask a question and provide the reponse within the callback.
@@ -9,16 +10,35 @@ params -
 - callback (function)(err, response) - The response.
 ]]--
 function ask(question, callback)
-  local response = ''
-  process.stdin:resume()
-  process.stdout:write(question .. ' ')
-  process.stdin:on('data', function(data)
-    response = response .. data
-    if response:find('\n') then
-      process.stdin:pause()
-      process.stdin:removeListener('data')
-      callback(nil, trim(response))
-    end
+  local resp = ''
+
+  function try(question, callback)
+    local response = ''
+    process.stdin:resume()
+    process.stdout:write(question .. ' ')
+    process.stdin:on('data', function(data)
+      response = response .. data
+      if response:find('\n') then
+        process.stdin:pause()
+        process.stdin:removeListener('data')
+        callback(nil, trim(response))
+      end
+    end)
+  end
+
+  function test()
+    return #resp == 0
+  end
+
+  function iter(callback)
+    try(question, function(err, data)
+      resp = data
+      callback(err, data)
+    end)
+  end
+
+  async.whilst(test, iter, function(err)
+    callback(err, resp)
   end)
 end
 
