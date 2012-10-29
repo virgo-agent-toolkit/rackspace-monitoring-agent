cedc2bbfbef5bddfb06f8e22b626ca720709e61f
Russell Haering <russell.haering@rackspace.com>
2012-10-29 16:10:10 -0700

    agents: monitoring: update the filesystem check test

--- agents/monitoring/tests/check/filesystem.lua
+++ agents/monitoring/tests/check/filesystem.lua
@@ -5,11 +5,11 @@ local FileSystemCheck = require('monitoring/default/check').FileSystemCheck
 local exports = {}
 
 exports['test_filesystem_check'] = function(test, asserts)
-  local check = FileSystemCheck:new({id='foo', period=30})
+  local check = FileSystemCheck:new({id='foo', period=30, details={target='/'}})
   asserts.ok(check._lastResult == nil)
   check:run(function(result)
     local util = require('utils')
-    local metrics = result:getMetrics()['/']
+    local metrics = result:getMetrics()['none']
 
     asserts.not_nil(metrics['total']['v'])
     asserts.not_nil(metrics['free']['v'])
