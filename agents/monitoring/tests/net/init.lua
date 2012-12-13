5115897160d4c0e061c289098d37754a8ec89017
Brandon Philips <brandon.philips@rackspace.com>
2012-12-13 14:55:37 -0800

    monitoring: tests: net: disable test_upgrades
    really disable test_upgrades this time

--- agents/monitoring/tests/net/init.lua
+++ agents/monitoring/tests/net/init.lua
@@ -73,8 +73,10 @@ end
 exports['test_upgrades'] = function(test, asserts)
   local options, client, endpoints
 
-  test.skip("Skip upgrades test until it is reliable")
-  return nil
+  if true then
+    test.skip("Skip upgrades test until it is reliable")
+    return nil
+  end
 
   -- Override the default download path
   consts.DEFAULT_DOWNLOAD_PATH = path.join('.', 'tmp')
