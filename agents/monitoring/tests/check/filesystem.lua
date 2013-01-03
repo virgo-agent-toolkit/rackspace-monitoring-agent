d263bcb39699df4eb6630b4668bc268034a8da38
Rob Emanuele <rob@emanuele.us>
2013-01-02 23:46:20 -0800

    Skip the filesystem tests on Windows but still call them individually.

--- agents/monitoring/tests/check/filesystem.lua
+++ agents/monitoring/tests/check/filesystem.lua
@@ -1,4 +1,5 @@
 local math = require('math')
+local os = require('os')
 
 local FileSystemCheck = require('monitoring/default/check').FileSystemCheck
 
@@ -50,6 +51,16 @@ exports['test_filesystem_check_no_mount_point'] = function(test, asserts)
     asserts.equal(result:getStatus(), 'Missing target parameter')
     test.done()
   end)
+end
+
+-- This will skip all the functions in the file but still call them individually
+if os.type() == "win32" then
+  for i,v in pairs(exports) do
+    p("Setting a skip " .. i .. " for " .. os.type())
+    exports[i] = function(test, asserts)
+      test.skip("Skipping " .. i .. " for " .. os.type())
+    end
+  end
 end
 
 return exports
