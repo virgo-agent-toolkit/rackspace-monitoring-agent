4c2c8961ed8daee10a902ca68a699f8508bd7d22
Rob Emanuele <rob@emanuele.us>
2013-01-26 22:58:59 -0800

    Windows Package Signing Python Script
    * use a Test Signing Cert or a specified signing cert
    * Test CA certicate files
    
    * break out the option loader into a seperate file
    * small clean up in configure

---
+++ tools/optloader.py
@@ -0,0 +1,30 @@
+import os
+import json
+import re
+
+root_dir = os.path.dirname(__file__)
+
+# Regular expression for comments
+comment_re = re.compile('^#(.+)$')
+
+def load_options():
+    options_filename = os.path.join(root_dir, '..', 'options.gypi')
+    print "reading ", options_filename
+
+    opts = {}
+    f = open(options_filename, 'rb')
+    content = ''
+    for line in f.readlines():
+        ## Looking for comments to remove
+        match = comment_re.search(line)
+        if match:
+            line = line[:match.start()] + line[match.end():]
+
+        content = content + line
+
+    opts = json.loads(content)
+    f.close()
+
+    return opts
+
+
---
+++ tools/win_sign.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+from optloader import load_options
+
+# Command set to create and sign with a test software certificate created from a test CA
+# "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\makecert.exe" -r -pe -n "CN=Rackspace Test CA" -a sha256 -cy authority -sky signature -sv testca.pvk testca.cer
+# "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\makecert.exe" -pe -n "CN=Rackspace Test Software Signing Cert" -a sha256 -cy end -sky signature -ic testca.cer -iv testca.pvk -sv testss.pvk testss.cer
+# "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\pvk2pfx.exe" -spc testss.cer -pvk testss.pvk -pfx testss.pfx
+# "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\signtool.exe" sign /d monitoring-agent.exe /v /f testss.pfx Debug/monitoring-agent.exe
+#
+# Test CA Cert Import
+# certutil -user -addstore Root testca.cer
+
+options = load_options();
+
+build = 'Debug' if options['variables']['virgo_debug'] == 'true' else 'Release'
+signtool = "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v7.0A\\bin\\signtool.exe"
+pfx = options['variables']['RACKSPACE_CODESIGNING_KEYFILE']
+
+result = -1
+
+if os.path.exists(pfx):
+    files_to_sign = [
+        "monitoring-agent.exe",
+        "rackspace-monitoring-agent.msi",
+    ]
+    for file in files_to_sign:
+        command = "\"%s\" sign /d \"%s\" /v /f \"%s\" \"%s\""%(signtool, file, pfx, build + "\\" + file)
+        result = subprocess.call(command, shell=True)
+        if result != 0:
+            print "FAILED(%d) CMD : %s"%(result, command)
+            break
+else:
+    print "No PFX found, looking for: %s"%pfx
+
+sys.exit(result)
