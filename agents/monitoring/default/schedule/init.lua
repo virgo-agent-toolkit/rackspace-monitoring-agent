1c4a45544c5ec01f5090c550a289bf5c6d5bec9d
Ryan Phillips <ryan.phillips@rackspace.com>
2013-02-26 10:18:17 -0600

    cleanup

--- agents/monitoring/default/schedule/init.lua
+++ agents/monitoring/default/schedule/init.lua
@@ -14,10 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 --]]
 
-local StateScanner = require('./scheduler').StateScanner
 local Scheduler = require('./scheduler').Scheduler
 
 local exports = {}
-exports.StateScanner = StateScanner
 exports.Scheduler = Scheduler
 return exports
