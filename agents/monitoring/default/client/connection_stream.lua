7e26aa1a6791e4d8fa7e9f9a3f8dd017dd8b237f
Ryan Phillips <ryan.phillips@rackspace.com>
2013-03-14 10:02:06 -0500

    swap < >

--- agents/monitoring/default/client/connection_stream.lua
+++ agents/monitoring/default/client/connection_stream.lua
@@ -101,7 +101,7 @@ function ConnectionStream:_onUpgrade()
     version = misc.trim(version)
     client:log(logging.DEBUG, fmt('(upgrade) -> Current Version: %s', bundleVersion))
     client:log(logging.DEBUG, fmt('(upgrade) -> Upstream Version: %s', version))
-    if misc.compareVersions(version, bundleVersion) < 0 then
+    if misc.compareVersions(version, bundleVersion) > 0 then
       client:log(logging.INFO, fmt('(upgrade) -> Performing upgrade to %s', version))
       self._messages:getUpgrade(version, client, function(err)
         if err then
