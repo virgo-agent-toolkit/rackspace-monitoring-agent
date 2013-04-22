51844ecc7af32f28d4f2b7e486c73fdf0a7cc300
Matt Kaniaris <mkaniaris@gmail.com>
2013-04-22 10:46:11 -0700

    Tests now run again, but do not pass

--- tools/paths.py
+++ tools/paths.py
@@ -11,12 +11,12 @@ BUNDLE_DIR = None
 def _abs_path(*args):
     return os.path.abspath(os.path.join(*args))
 
-_options = optloader.load_options('platform.gypi')
+_options = optloader.load_options('platform.gypi')['variables']
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BUNDLE_DIR = _abs_path(ROOT, _options['variables']['BUNDLE_DIR'])
-BUNDLE_NAME = _options['variables']['BUNDLE_NAME']
+BUNDLE_DIR = _abs_path(ROOT, _options['BUNDLE_DIR'])
+BUNDLE_NAME = _options['BUNDLE_NAME']
 
 if sys.platform == "win32":
     #TEMPORARY HACK - this is in options.gypi
@@ -28,7 +28,7 @@ else:
     BUILDTYPE = os.environ.get('BUILDTYPE', 'Debug')
     BUILD_DIR = _abs_path(ROOT, 'out', BUILDTYPE)
     LUVIT = _abs_path(BUILD_DIR, 'luvit')
-    AGENT = _abs_path(BUILD_DIR, 'virgo')
+    AGENT = _abs_path(BUILD_DIR, _options['PKG_NAME'])
 
 
 if __name__ == "__main__":
