0847733ced59a9f96139a8041672ae23ac485dde
Rob Emanuele <rob@emanuele.us>
2013-04-24 18:53:35 -0700

    Windows Operating System Performance Data Check
    * Exposes the Win32_PerfFormattedData_PerfOS_System via the PowerShell
    * Converts the Win32_PerfFormattedData_PerfOS_System data into a CSV
    * Uses the CSV to populate the check metrics
    * Expose the WMI data-types in the CSV and map them to virgo types
    
    * Write a test case to try it on Windows, and error according on other OSes

--- agents/monitoring/default/util/misc.lua
+++ agents/monitoring/default/util/misc.lua
@@ -275,6 +275,47 @@ function copyFile(fromFile, toFile, callback)
 end
 
 
+function parseCSVLine (line,sep) 
+  local res = {}
+  local pos = 1
+  sep = sep or ','
+  while true do 
+    local c = string.sub(line,pos,pos)
+    if (c == "") then break end
+    if (c == '"') then
+      -- quoted value (ignore separator within)
+      local txt = ""
+      repeat
+        local startp,endp = string.find(line,'^%b""',pos)
+        txt = txt..string.sub(line,startp+1,endp-1)
+        pos = endp + 1
+        c = string.sub(line,pos,pos) 
+        if (c == '"') then txt = txt..'"' end 
+        -- check first char AFTER quoted string, if it is another
+        -- quoted string without separator, then append it
+        -- this is the way to "escape" the quote char in a quote. example:
+        --   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+      until (c ~= '"')
+      table.insert(res,txt)
+      assert(c == sep or c == "")
+      pos = pos + 1
+    else	
+      -- no quotes used, just look for the first separator
+      local startp,endp = string.find(line,sep,pos)
+      if (startp) then 
+        table.insert(res,string.sub(line,pos,startp-1))
+        pos = endp + 1
+      else
+        -- no separator found -> use rest of string and terminate
+        table.insert(res,string.sub(line,pos))
+        break
+      end 
+    end
+  end
+  return res
+end
+
+
 --[[ Exports ]]--
 local exports = {}
 exports.copyFile = copyFile
@@ -291,4 +332,5 @@ exports.fireOnce = fireOnce
 exports.nCallbacks = nCallbacks
 exports.compareVersions = compareVersions
 exports.propagateEvents = propagateEvents
+exports.parseCSVLine = parseCSVLine
 return exports
