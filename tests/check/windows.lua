e2bfea46ffe0e6a88d22531edd1fc1cfa84c860c
Rob Emanuele <rje@bitstruct.com>
2013-05-26 00:30:23 +0000

    create multiple test_sqlserver_checks based on an external config file to specify the server to test against

--- tests/check/windows.lua
+++ tests/check/windows.lua
@@ -1,5 +1,6 @@
 local math = require('math')
 local os = require('os')
+local string = require('string')
 local helper = require('../helper')
 
 local WindowsPerfOSCheck = require('/check/windows').checks.WindowsPerfOSCheck
@@ -29,27 +30,32 @@ exports['test_windowsperfos_check'] = function(test, asserts)
   end)
 end
 
-exports['test_sqlserver_check'] = function(test, asserts)
-  if helper.test_configs['sqlserver12'] == nil then
-    test.skip('test_sqlserver_check requires a config file')
-  else
-    local check = MSSQLServerDatabaseCheck:new({id='foo', period=30, details=helper.test_configs['sqlserver12']})
-    asserts.ok(check._lastResult == nil)
-    check:run(function(result)
-      asserts.ok(result ~= nil)
-      asserts.ok(check._lastResult ~= nil)
+if helper.test_configs['test_sqlserver_check'] == nil then
+  exports['test_sqlserver_check'] = function(test, asserts)
+    test.skip('test_sqlserver_check requires at least one config file entry')
+  end
+else
+  for i, config in ipairs(helper.test_configs['test_sqlserver_check']) do
+    local name = string.format('test_sqlserver_check_%u', i)
+    exports[name] = function(test, asserts)
+      local check = MSSQLServerDatabaseCheck:new({id='foo', period=30, details=config})
+      asserts.ok(check._lastResult == nil)
+      check:run(function(result)
+        asserts.ok(result ~= nil)
+        asserts.ok(check._lastResult ~= nil)
 
-      if os.type() == 'win32' then
-        asserts.ok(result:getStatus() == 'success')
-        asserts.ok(#check._lastResult:serialize() > 0)
-        local metrics = result:getMetrics()['none']
-        -- Values always become strings internally
-        asserts.ok(tonumber(metrics['size']['v']) > 0)
-      else
-        asserts.ok(result:getStatus() ~= 'success')
-      end
-      test.done()
-    end)
+        if os.type() == 'win32' then
+          asserts.ok(result:getStatus() == 'success')
+          asserts.ok(#check._lastResult:serialize() > 0)
+          local metrics = result:getMetrics()['none']
+          -- Values always become strings internally
+          asserts.ok(tonumber(metrics['size']['v']) > 0)
+        else
+          asserts.ok(result:getStatus() ~= 'success')
+        end
+        test.done()
+      end)
+    end
   end
 end
 
