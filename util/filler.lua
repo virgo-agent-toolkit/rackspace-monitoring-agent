194afb2e35f26550c43170098e2f25d44210f400
Rob Emanuele <rob@emanuele.us>
2014-01-27 19:31:26 -0800

    db obj filler full implementation

--- util/filler.lua
+++ util/filler.lua
@@ -18,6 +18,8 @@ local Emitter = require('core').Emitter
 local loggingUtil = require('/util/logging')
 local logging = require('logging')
 local fmt = require('string').format
+local misc = require('/util/misc')
+local table = require('table')
 
 local Filler = Emitter:extend()
 
@@ -27,6 +29,12 @@ function Filler:initialize(conn, entity, limit)
   self._limit = limit or 100
   self.logger = loggingUtil.makeLogger('Filler')
   self.db_list_order = { 'check', 'notification_plan', 'alarm', 'notification' }
+  self._data = {}
+  self.db_list_done = {}
+end
+
+function Filler:getRetrievedData()
+  return self._data
 end
 
 function Filler:start()
@@ -48,6 +56,7 @@ function Filler:start()
   local _, now_obj_type
   for _, now_obj_type in ipairs(self.db_list_order) do
     self.logger(logging.INFO, fmt('retrieving objects marked as: %s', now_obj_type))
+    self._data[now_obj_type] = {}
     xpcall( function()
       self:_list_handler(now_obj_type, db_listers[now_obj_type])
     end, function(err)
@@ -59,7 +68,7 @@ end
 function Filler:_amidone(obj_type)
   self.db_list_done[obj_type] = true
   local _, now_obj_type
-  for _, now_obj_type in ipairs(self.db_sync_order) do
+  for _, now_obj_type in ipairs(self.db_list_order) do
     if not self.db_list_done[now_obj_type] then
       return
     end
@@ -68,27 +77,36 @@ function Filler:_amidone(obj_type)
 end
 
 function Filler:_list_handler(obj_type, listfunc)
-  self:on(obj_type .. "_end", function(obj_type)
+  self:on(obj_type .. "_end", function()
+    self.logger(logging.INFO, fmt('completed retrieving objects marked as: %s', obj_type))
     self:_amidone(obj_type)
   end)
   self:on(obj_type .. "_data", function(err, data)
-    p(obj_type .. " list", data)
-    p(obj_type .. " results", data.result.values, data.result.metadata)
     --sort out data
     if not err and data.result then
       if data.result.values then
-        self._data[obj_type] = misc.merge(self._data[obj_type], data.result.values)
-      end
-      --get next data if needed
-      if data.result.metadata then
-        if data.result.metadata.marker or data.result.metadata.start then
-          --get next data from marker on
-          listfunc(self, data.result.marker, function (err, data)
-            self:emit(obj_type .. "_data", err, data)
-          end)
-        else
-          self:emit(obj_type .. "_end")
+        local _, val
+        for _, val in ipairs(data.result.values) do
+          table.insert(self._data[obj_type], val)
         end
+      end
+
+      --get next data if needed
+      local next_key, start
+      pcall(function()
+        start = data.result.metadata.start
+      end)
+      pcall(function()
+        next_key = data.result.metadata.next_marker.nextKey
+      end)
+
+      if next_key or start then
+        --get next data from marker on
+        listfunc(self, next_key, function (err, data)
+          self:emit(obj_type .. "_data", err, data)
+        end)
+      else
+        self:emit(obj_type .. "_end")
       end
     end
   end)
