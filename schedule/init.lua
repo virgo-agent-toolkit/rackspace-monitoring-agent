262f7fb12de6cb0d86c5286249bb2cd33af6e7ea
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-06 19:32:41 -0600

    add license and tweak init.lua for scheduler

--- schedule/init.lua
+++ schedule/init.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2012 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,8 +14,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 --]]
 
-local Scheduler = require('./scheduler').Scheduler
-
-local exports = {}
-exports.Scheduler = Scheduler
-return exports
+return require('./scheduler')
