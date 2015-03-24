165ebba80f4293766bde44b5e78ec22c024f7ff2
Ryan Phillips <ryan@trolocsis.com>
2015-03-24 09:19:40 -0500

    Update emitter.lua

--- lua_modules/line-emitter/lib/emitter.lua
+++ lua_modules/line-emitter/lib/emitter.lua
@@ -1,6 +1,5 @@
 --[[
 Copyright 2015 Virgo Agent Toolkit Authors
-Copyright Tomaz Muraus
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
