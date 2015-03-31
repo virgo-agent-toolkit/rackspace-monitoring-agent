563ac33a451d267772200fd9bc25180823f9db2a
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-31 12:34:41 -0500

    use the correct debian repo name

--- cmake/reprepro.cmake
+++ cmake/reprepro.cmake
@@ -1,5 +1,7 @@
-set(REPO_DIRECTORY ${CMAKE_BINARY_DIR}/repository)
-configure_file(${REPOSITORY_SCRIPTS}/reprepro.in ${REPO_DIRECTORY}/conf/distributions)
+set(REPO_NAME ${LINUX_NAME_LOWER}-${LINUX_VER}-${CMAKE_SYSTEM_PROCESSOR})
+set(REPO_PATH ${CMAKE_BINARY_DIR}/${REPO_NAME})
+message("Creating ${REPO_NAME} Repository")
+configure_file(${REPOSITORY_SCRIPTS}/reprepro.in ${REPO_PATH}/conf/distributions)
 add_custom_target(packagerepo
-  COMMAND reprepro -b ${REPO_DIRECTORY} includedeb cloudmonitoring *.deb
+  COMMAND reprepro -b ${REPO_PATH} includedeb cloudmonitoring *.deb
 )
