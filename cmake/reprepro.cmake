aee145c2fa2da2d664f41648799cf48d21e8986b
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-31 12:52:14 -0500

    add REPO_RPM_NAME

--- cmake/reprepro.cmake
+++ cmake/reprepro.cmake
@@ -3,5 +3,5 @@ set(REPO_PATH ${CMAKE_BINARY_DIR}/${REPO_NAME})
 message("Creating ${REPO_NAME} Repository")
 configure_file(${REPOSITORY_SCRIPTS}/reprepro.in ${REPO_PATH}/conf/distributions)
 add_custom_target(packagerepo
-  COMMAND reprepro -b ${REPO_PATH} includedeb cloudmonitoring *.deb
+  COMMAND reprepro -b ${REPO_PATH} includedeb ${REPO_RPM_NAME} *.deb
 )
