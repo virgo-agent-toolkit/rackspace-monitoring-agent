9cc1a20047c2de9d610fd8c49c15e257bdb8ca6e
Ryan Phillips <ryan.phillips@rackspace.com>
2015-05-14 21:43:16 -0500

    bump to lit 1.2.7

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 APP_FILES=$(shell find . -type f -name '*.lua')
-LIT_VERSION=1.2.6
+LIT_VERSION=1.2.7
 TARGET=rackspace-monitoring-agent
 LUVI?=./luvi
 LIT?=./lit
--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "rackspace-monitoring-agent",
-  version = "1.11.6",
+  version = "1.12.0",
   dependencies = {
     "rphillips/options@0.0.5",
     "virgo-agent-toolkit/rackspace-monitoring-client@0.3",
