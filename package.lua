2c2eb849292d0a1f516d46315f8fb3778a4f81d2
Ryan Phillips <ryan.phillips@rackspace.com>
2015-06-05 16:46:00 -0500

    bump to 1.9.5

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "rackspace-monitoring-agent",
-  version = "1.9.0",
+  version = "1.9.5",
   dependencies = {
     "rphillips/options@0.0.5",
     "virgo-agent-toolkit/rackspace-monitoring-client@0.3",
