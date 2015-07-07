f73a0b200dde2df0c59cd0a4e450cde37a880143
Ryan Phillips <ryan.phillips@rackspace.com>
2015-07-07 09:03:09 -0500

    sort constants

--- constants.lua
+++ constants.lua
@@ -27,16 +27,18 @@ local SNET_REGIONS = {
 }
 
 local constants = const.ConstantsCtx:new()
-constants:setGlobal('DEFAULT_CUSTOM_PLUGINS_PATH', path.join(const.LIBRARY_DIR, 'plugins'))
-constants:setGlobal('DEFAULT_PLUGIN_TIMEOUT', 60 * 1000)
-constants:setGlobal('PLUGIN_TYPE_MAP', {string = 'string', int = 'int64', float = 'double', gauge = 'gauge'})
-constants:setGlobal('CRASH_REPORT_URL', 'https://monitoring.api.rackspacecloud.com/agent-crash-report')
-constants:setGlobal('DEFAULT_PID_FILE_PATH', '/var/run/rackspace-monitoring-agent.pid')
-constants:setGlobal('DEFAULT_LOCK_FILE_PATH', '/var/run/rackspace-monitoring-agent.lock')
+constants:setGlobal('CHECK_SCHEDULE_JITTER', 15000) -- milliseconds
 constants:setGlobal('DEFAULT_CONFD_PATH', '/etc/rackspace-monitoring-agent.conf.d')
-constants:setGlobal('VALID_SNET_REGION_NAMES', SNET_REGIONS)
+constants:setGlobal('DEFAULT_CUSTOM_PLUGINS_PATH', path.join(const.LIBRARY_DIR, 'plugins'))
+constants:setGlobal('DEFAULT_LOCK_FILE_PATH', '/var/run/rackspace-monitoring-agent.lock')
+constants:setGlobal('DEFAULT_PID_FILE_PATH', '/var/run/rackspace-monitoring-agent.pid')
+constants:setGlobal('DEFAULT_PLUGIN_TIMEOUT', 60 * 1000)
+constants:setGlobal('MAX_CHECK_PERIOD', 1800000) -- 30 minute check period (milliseconds)
+constants:setGlobal('METRIC_STATUS_MAX_LENGTH', 256)
+constants:setGlobal('PLUGIN_TYPE_MAP', {string = 'string', int = 'int64', float = 'double', gauge = 'gauge'})
 constants:setGlobal('SETUP_AUTH_CHECK_INTERVAL', 1000)
 constants:setGlobal('SETUP_AUTH_TIMEOUT', 10000)
+constants:setGlobal('VALID_SNET_REGION_NAMES', SNET_REGIONS)
 
 constants:setGlobal('DEFAULT_MONITORING_SRV_QUERIES_STAGING', {
   '_monitoringagent._tcp.dfw1.stage.monitoring.api.rackspacecloud.com',
@@ -61,10 +63,5 @@ constants:setGlobal('SNET_MONITORING_TEMPLATE_SRV_QUERIES', {
   '_monitoringagent._tcp.snet-${region}-region1.prod.monitoring.api.rackspacecloud.com',
   '_monitoringagent._tcp.snet-${region}-region2.prod.monitoring.api.rackspacecloud.com'
 })
-
-constants:setGlobal('METRIC_STATUS_MAX_LENGTH', 256)
-
-constants:setGlobal('MAX_CHECK_PERIOD', 1800000) -- 30 minute check period (milliseconds)
-constants:setGlobal('CHECK_SCHEDULE_JITTER', 15000) -- milliseconds
 
 return constants
