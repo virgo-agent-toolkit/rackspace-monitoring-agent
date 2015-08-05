a3e1991263fab3b9434500126f9073b3a4f81aa8
Ryan Phillips <ryan.phillips@rackspace.com>
2015-08-05 12:14:06 -0500

    change readCast to stream

--- hostinfo/misc.lua
+++ hostinfo/misc.lua
@@ -64,44 +64,32 @@ local function readCast(filePath, errTable, outTable, casterFunc, callback)
   if (type(casterFunc) ~= 'function') then function casterFunc(iter, obj, line) end end
   if (type(callback) ~= 'function') then function callback() end end
 
-  local obj = {}
-  fs.exists(filePath, function(err, file)
+  local function onExists(err, file)
     if err then
       table.insert(errTable, string.format('File not found : { fs.exists erred: %s }', err))
       return callback()
     end
-    if file then
-      fs.readFile(filePath, function(err, data)
-
-        if err then
-          table.insert(errTable, string.format('File couldnt be read : { fs.readline erred: %s }', err))
-          return callback()
-        end
-
-        for line in data:gmatch("[^\r\n]+") do
-          local iscomment = string.match(line, '^#')
-          local isblank = string.len(line:gsub("%s+", "")) <= 0
-
-          if not iscomment and not isblank then
-            -- split the line and assign key vals
-            local iter = line:gmatch("%S+")
-            casterFunc(iter, obj, line)
-          end
-        end
-
-        -- Flatten single entry objects
-        if #obj == 1 then obj = obj[1] end
-        -- Dont insert empty objects into the outTable
-        if next(obj) then table.insert(outTable, obj) end
-
-        return callback()
-      end)
-    else
-      table.insert(errTable, 'file not found')
+    local obj = {}
+    local stream = fs.createReadStream(filePath)
+    local le = LineEmitter:new()
+    le:on('data', function(line)
+      local iscomment = string.match(line, '^#')
+      local isblank = string.len(line:gsub("%s+", "")) <= 0
+      if not iscomment and not isblank then
+        -- split the line and assign key vals
+        local iter = line:gmatch("%S+")
+        casterFunc(iter, obj, line)
+      end
+    end)
+    stream:pipe(le):once('end', function()
+      -- Flatten single entry objects
+      if #obj == 1 then obj = obj[1] end
+      -- Dont insert empty objects into the outTable
+      if next(obj) then table.insert(outTable, obj) end
       return callback()
-    end
-
-  end)
+    end)
+  end
+  fs.exists(filePath, onExists)
 end
 
 local function asyncSpawn(dataArr, spawnFunc, successFunc, finalCb)
