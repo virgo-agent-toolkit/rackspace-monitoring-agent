ecb16a35ae71e6c0094790fd1f6e212b0dc56d9e
Ryan Phillips <ryan.phillips@rackspace.com>
2015-08-05 08:38:01 -0500

    refactor hostinfo

--- hostinfo/base.lua
+++ hostinfo/base.lua
@@ -14,12 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 --]]
 
-local Object = require('core').Object
-local vutils = require('virgo/utils')
+local Transform = require('stream').Transform
+local execFileToStreams = require('./misc').execFileToStreams
+local gmtNow = require('virgo/utils').gmtNow
+local los = require('los')
 
---[[ HostInfo ]]--
-local HostInfo = Object:extend()
+-------------------------------------------------------------------------------
+
+local HostInfo = Transform:extend()
 function HostInfo:initialize()
+  Transform.initialize(self, {objectMode = true})
   self._params = {}
   self._error = nil
 end
@@ -28,7 +32,7 @@ function HostInfo:serialize()
   return {
     error = self._error,
     metrics = self._params,
-    timestamp = vutils.gmtNow()
+    timestamp = gmtNow()
   }
 end
 
@@ -37,3 +41,66 @@ function HostInfo:run(callback)
 end
 
 exports.HostInfo = HostInfo
+
+-------------------------------------------------------------------------------
+
+local HostInfoStdoutSubProc = HostInfo:extend()
+function HostInfoStdoutSubProc:initialize(command, args)
+  HostInfo.initialize(self)
+  self.command = command
+  self.args = args or {}
+end
+
+function HostInfoStdoutSubProc:getPlatforms()
+  return {}
+end
+
+function HostInfoStdoutSubProc:_execute(callback)
+  local exitCode
+  local called = 2
+  local function done()
+    called = called - 1
+    if called == 0 then
+      if exitCode ~= 0 then
+        self._error = 'Process exited with exit code ' .. exitCode
+      end
+      callback()
+    end
+  end
+  local function onClose(_exitCode)
+    exitCode = _exitCode
+    done()
+  end
+  self.child, self.stdout, self.stderr = execFileToStreams(self.command,
+                                                           self.args,
+                                                           { env = process.env })
+  self.child:once('close', onClose)
+  self.stdout:pipe(self)
+    :on('data', function(obj)
+      table.insert(self._params, obj)
+    end)
+    :once('end', done)
+end
+
+function HostInfoStdoutSubProc:run(callback)
+  local currentPlatform = los.type()
+  local found = false
+  for _, platform in pairs(self:getPlatforms()) do
+    if platform == currentPlatform then
+      found = true
+      break
+    end
+  end
+  if not found then
+    self._error = 'unsupported operating system for ' .. self:getType()
+    return callback()
+  end
+  -- assume if command is nil that this vendor is not supported
+  if not self.command then
+    self._error = string.format('unsupported operating system for %s hostinfo', self:getType())
+    return callback()
+  end
+  self:_execute(callback)
+end
+
+exports.HostInfoStdoutSubProc = HostInfoStdoutSubProc
