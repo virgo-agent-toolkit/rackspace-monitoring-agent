5450c3b3e87d0d257ac5bc2a281268a0e8a076e8
Ryan Phillips <ryan.phillips@rackspace.com>
2015-08-05 08:38:01 -0500

    refactor hostinfo

--- hostinfo/sysctl.lua
+++ hostinfo/sysctl.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2014 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,55 +13,25 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 --]]
-local HostInfo = require('./base').HostInfo
 
-local fmt = require('string').format
-local table = require('table')
-local los = require('los')
-local spawn = require('childprocess').spawn
+local HostInfoStdoutSubProc = require('./base').HostInfoStdoutSubProc
 
---[[ Sysctl Variables ]]--
-local Info = HostInfo:extend()
+local Info = HostInfoStdoutSubProc:extend()
 function Info:initialize()
-  HostInfo.initialize(self)
+  HostInfoStdoutSubProc.initialize(self, 'sysctl', {'-A'})
 end
 
-function Info:run(callback)
-  if los.type() ~= 'linux' then
-    self._error = 'Unsupported OS for sysctl'
-    callback()
-    return
+function Info:_transform(line, callback)
+  line = line:gsub("^%s*(.-)%s*$", "%1")
+  local _, _, key, value = line:find("([^=^%s]+)%s*=%s*([^=]*)")
+  if key and value then
+    self:push({ [key] = value })
   end
+  callback()
+end
 
-  local child = spawn('sysctl', {'-A'}, {})
-  local data = ''
-
-  child.stdout:on('data', function(chunk)
-    data = data .. chunk
-  end)
-
-  child:on('exit', function(exit_code)
-    if exit_code ~= 0 then
-      self._error = fmt("sysctl exited with a %d exit_code", exit_code)
-      callback()
-      return
-    end
-    for line in data:gmatch("[^\r\n]+") do
-      line = line:gsub("^%s*(.-)%s*$", "%1")
-      local _, _, key, value = line:find("([^=^%s]+)%s*=%s*([^=]*)")
-      if key ~= nil then
-        local obj = {}
-        obj[key] = value
-        table.insert(self._params, obj)
-      end
-    end
-    callback()
-  end)
-
-  child:on('error', function(err)
-    self._error = err
-    callback()
-  end)
+function Info:getPlatforms()
+  return {'linux', 'darwin', 'freebsd', 'openbsd', 'netbsd'}
 end
 
 function Info:getType()
