ea5e2e95e47513e9bf8cc4dd35271da8649f01e4
Ryan Phillips <ryan.phillips@rackspace.com>
2015-11-16 15:22:42 -0600

    bump to 2.2.3

--- package.lua
+++ package.lua
@@ -1,8 +1,8 @@
 return {
   name = "rackspace-monitoring-agent",
-  version = "2.2.2",
+  version = "2.2.3",
   luvi = {
-    version = "2.3.4-sigar",
+    version = "2.3.5-sigar",
     flavor = "sigar",
     url = "https://github.com/virgo-agent-toolkit/luvi/releases/download/v%s-sigar/luvi-%s-%s"
   },
