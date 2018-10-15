267c50e28a89eb5cffc9910bbaea821f623bb4bb
James Buchan <james.buchan@rackspace.com>
2018-10-15 14:17:36 -0600

    Bump to 2.6.19

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "rackspace-monitoring-agent",
-  version = "2.6.18",
+  version = "2.6.19",
   luvi = {
     version = "2.7.6-2-sigar",
     flavor = "sigar",
@@ -9,7 +9,7 @@ return {
   dependencies = {
     "rphillips/options@0.0.5",
     "virgo-agent-toolkit/rackspace-monitoring-client@0.3",
-    "virgo-agent-toolkit/virgo@2.1.9",
+    "virgo-agent-toolkit/virgo@2.1.10",
     "kaustavha/luvit-walk@1",
   },
   files = {
